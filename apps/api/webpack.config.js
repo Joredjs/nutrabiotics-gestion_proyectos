c8838f470c9925e60a3c0c245058f42f5e021b44
Jorge Garay <odraude160@gmail.com>
2025-06-20 11:34:22 -0500

    chore: create backend apps

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/nutrabiotics-system'),
+    path: join(__dirname, '../../dist/api'),
   },
   plugins: [
     new NxAppWebpackPlugin({
